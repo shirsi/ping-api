0cac48bc2710cfb2a074923966e2b363c3f35cb3
Safia Hirsi <safa.hirsi@gmail.com>
2020-05-21 23:10:53 -0500

    update server

--- server.js
+++ server.js
@@ -1,6 +1,43 @@
 const http = require("http");
 const app = require("./app.js");
-const server = http.createServer(app);
-const port = process.env.PORT || 3000;
-app.set("port", port);
-server.listen(port);
+
+const normalizePort = (val) => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // name pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = (error) => {
+  if (errror.syscall !== "listen") {
+    throw errror;
+  }
+  const bind = typeof addr === "string" ? "pipe" + addr : "port" + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + "require elevated priviages");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " require elevated privilages");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? " pipe " + addr : "port " + port;
+  debug("Listening on + " + bind);
+};
