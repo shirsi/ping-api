ca5019872b8e9e0c024d11780e4cc4c11462fc97
Safia Hirsi <safa.hirsi@gmail.com>
2020-05-22 20:35:32 -0500

    update server listening

--- server.js
+++ server.js
@@ -41,3 +41,13 @@ const onListening = () => {
   const bind = typeof addr === "string" ? " pipe " + addr : "port " + port;
   debug("Listening on + " + bind);
 };
+
+const port = normalizePort(process.env.PORT || "3000");
+
+app.sert("port", port);
+
+const srver = http.createServer(app);
+server.on("error", onError);
+
+server.on("listening", onListening);
+server.listen(port);
